9a13f45cd34c0d558aaa53e6402be0ab4773b18d
rogri2 <rodyap182@gmail.com>
2021-10-11 22:16:03 -0500

    Principio del login
    Se cambió el constructor del UsuarioController para que se pudiera usar tanto en login como registro; se implementó el principio del login

--- routes/Usuarios/loginUsuarios.php
+++ routes/Usuarios/loginUsuarios.php
@@ -6,8 +6,15 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 
 header("Access-Control-Allow-Headers: X-Requested-With");
-$endPoint = new UsuarioController();
+$endPoint = new UsuarioController('../../config/db.php');
 $json_data = file_get_contents('php://input');
 $model = json_decode($json_data);
 
-$endPoint->loginUsuario($model, $model);
+$correo = $model->correo;
+$contra = $model->contra;
+
+//foreach($model as $key=>$value) {
+//    echo $key . "=>" . $value;
+//}
+
+$endPoint->loginUsuario($correo, $contra);
