a5b39519113a67f7002fb2d1e07e611c894610b8
Luis Villanueva <luis.villanueva.1029@gmail.com>
2021-10-12 21:50:54 -0500

    se agrego el obtener usuario por Id

---
+++ routes/Usuarios/updateUsuario.php
@@ -0,0 +1,20 @@
+<?php
+require_once '../../controller/usuarioController.php';
+// Allow CORS
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Credentials: true');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("Access-Control-Allow-Headers: *");
+}
+$endPoint = new UsuarioController("../../config/db.php", "../../model/usuario.php");
+$json_data = file_get_contents('php://input');
+
+$data = json_decode($json_data);
+
+$correo = $data->correo;
+$contra = $data->contra;
+
+echo json_encode($endPoint->loginUsuario($correo, $contra));
