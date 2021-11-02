1f70e615058b2b89f2d034331b48ad58bcac40ea
Rodrigo Yap <rodyap182@gmail.com>
2021-11-01 20:55:07 -0600

    Se agregaron rutas

---
+++ routes/Categorias/getCategoria.php
@@ -0,0 +1,14 @@
+<?php
+require_once '../../controller/categoriaController.php';
+// Allow CORS
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
+header("Access-Control-Allow-Origin: *");
+header('content-type: application/json; charset=utf-8');
+$endPoint = new CategoriaController("../../config/db.php", "../../model/categoria.php");
+$json_data = file_get_contents('php://input');
+
+//$data = json_decode($json_data);
+
+//$id = $data->idUsuario;
+
+echo json_encode($endPoint->getUsuario());
---
+++ routes/Categorias/registrarCategoria.php
@@ -0,0 +1,15 @@
+<?php
+require '../controller/categoriaController.php';
+require_once '../model/categoria.php';
+// Allow CORS
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
+header("Access-Control-Allow-Origin: *");
+header('content-type: application/json; charset=utf-8');
+$endPoint = new CategoriaController("../config/db.php", "../model/categoria.php");
+$json_data = file_get_contents('php://input');
+
+$data = json_decode($json_data);
+
+$nombre = $data->nombre;
+
+$endPoint->registrarCategoria($nombre);
