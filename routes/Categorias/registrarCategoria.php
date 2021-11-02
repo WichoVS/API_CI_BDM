416982ba23502824d89f900e99b13e2a8cf445c5
Rodrigo Yap <rodyap182@gmail.com>
2021-11-01 21:31:24 -0600

    Update registrarCategoria.php

--- routes/Categorias/registrarCategoria.php
+++ routes/Categorias/registrarCategoria.php
@@ -1,11 +1,11 @@
 <?php
-require '../controller/categoriaController.php';
-require_once '../model/categoria.php';
+require '../../controller/categoriaController.php';
+require_once '../../model/categoria.php';
 // Allow CORS
 header("Access-Control-Allow-Headers: Authorization, Content-Type");
 header("Access-Control-Allow-Origin: *");
 header('content-type: application/json; charset=utf-8');
-$endPoint = new CategoriaController("../config/db.php", "../model/categoria.php");
+$endPoint = new CategoriaController("../../config/db.php", "../../model/categoria.php");
 $json_data = file_get_contents('php://input');
 
 $data = json_decode($json_data);
